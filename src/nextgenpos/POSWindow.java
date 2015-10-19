54e25cc45eab1a61945870b2168fa6ed61c1c770
Forhan Noor <forhannoor@gmail.com>
2015-10-19 21:55:30 +0600

    POSWindow being redeigned from scratch

--- src/nextgenpos/POSWindow.java
+++ src/nextgenpos/POSWindow.java
@@ -11,11 +11,12 @@ public class POSWindow extends JFrame implements ActionListener{
 	private JTable salesLine;
 	
 	private JTextField barcode;
-	private JTextField quantity;
 	private JTextField total;
 	private JTextField vat;
 	private JTextField discount;
 	private JTextField netTotal;
+	
+	private JSpinner quantity;
 	
 	private JButton start;
 	private JButton end;
@@ -23,18 +24,18 @@ public class POSWindow extends JFrame implements ActionListener{
 	private JButton discard;
 	
 	public POSWindow(){
-		setLayout(new GridLayout(2, 1)); // frame layout
+		setLayout(new GridLayout(2, 1, 0, 5)); // frame layout
 		
 		JPanel top = new JPanel(); // contains sales line
 		top.setLayout(new GridLayout(1, 1));
 		JPanel bottom = new JPanel(); // contains 3 more panels
-		bottom.setLayout(new GridLayout(1, 3));
+		bottom.setLayout(new GridLayout(1, 3, 20, 0));
 		JPanel bottomFirstColumn = new JPanel(); // contains start and end buttons 
 		bottomFirstColumn.setLayout(new GridLayout(2, 1));
 		JPanel bottomSecondColumn = new JPanel(); // contains fields to add/discard product 
 		bottomSecondColumn.setLayout(new GridLayout(4, 2));
 		JPanel bottomThirdColumn = new JPanel(); // contains total, vat, discount and netTotal fields
-		bottomThirdColumn.setLayout(new GridLayout(3, 2));
+		bottomThirdColumn.setLayout(new GridLayout(4, 2));
 		
 		// initialize top panel component
 		salesLine = new JTable();
@@ -42,13 +43,46 @@ public class POSWindow extends JFrame implements ActionListener{
 		
 		// initialize first column of bottom panel
 		start = new JButton("Start Sale");
+		start.addActionListener(this);
 		end = new JButton("End Sale");
+		end.addActionListener(this);
 		bottomFirstColumn.add(start);
 		bottomFirstColumn.add(end);
 		
 		// initialize second column of bottom panel
+		barcode = new JTextField();
+		SpinnerNumberModel model = new SpinnerNumberModel(1.0, 0.5, 100.00, 1.0);  
+		quantity = new JSpinner(model);
+		add = new JButton("Add");
+		add.addActionListener(this);
+		discard = new JButton("Discard");
+		discard.addActionListener(this);
+		bottomSecondColumn.add(new JLabel("Barcode"));
+		bottomSecondColumn.add(barcode);
+		bottomSecondColumn.add(new JLabel("Quantity"));
+		bottomSecondColumn.add(quantity);
+		bottomSecondColumn.add(new JLabel());
+		bottomSecondColumn.add(add);
+		bottomSecondColumn.add(new JLabel());
+		bottomSecondColumn.add(discard);
 		
 		// initialize third column of bottom panel
+		total = new JTextField();
+		total.setEditable(false);
+		vat = new JTextField();
+		vat.setEditable(false);
+		discount = new JTextField();
+		discount.setEditable(false);
+		netTotal = new JTextField();
+		netTotal.setEditable(false);
+		bottomThirdColumn.add(new JLabel("Total"));
+		bottomThirdColumn.add(total);
+		bottomThirdColumn.add(new JLabel("VAT"));
+		bottomThirdColumn.add(vat);
+		bottomThirdColumn.add(new JLabel("Discount"));
+		bottomThirdColumn.add(discount);
+		bottomThirdColumn.add(new JLabel("Net Total"));
+		bottomThirdColumn.add(netTotal);
 		
 		// initialize bottom panel components
 		bottom.add(bottomFirstColumn);
