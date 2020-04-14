7048bcf88c38b938f327ac9f56db44d6efee71f9
mac_bd@yahoo.com <forhannoor@gmail.com>
2020-04-13 21:49:06 -0400

    Payment.java refactored

--- src/nextgenpos/Payment.java
+++ src/nextgenpos/Payment.java
@@ -2,134 +2,138 @@
 
 package nextgenpos;
 
-import java.awt.Font;
+//import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-
 import javax.swing.*;
 
-public class Payment extends JFrame implements ActionListener, ItemListener{
+enum PaymentMethod{
+	None,
+	Cash,
+	Card
+}
 
-	private JComboBox paymentMethods;
+public class Payment extends JFrame implements ActionListener, ItemListener{
+	private JComboBox<PaymentMethod> paymentMethodsComboBox;
+	private JTextField cardNoField;
+	private JTextField dueField;
+	private JTextField paidField;
+	private JTextField changeField;
+	private JButton processButton;
 	
-	private JTextField cardNo;
-	private JTextField due;
-	private JTextField paid;
-	private JTextField change;
-	
-	private JButton process;
+	private final int WIDTH = 500;
+	private final int HEIGHT = 500;
+	private final int LOCATION = 100;
+	private final String BLANK = "";
+	private final String CARD_NO = "Card No.";
+	private final String DUE = "Due";
+	private final String PAID = "Paid";
+	private final String CHANGE = "Change";
+	private final String TITLE = "Payment Processing";
+	private final String PROCESS = "Process";
+	private final String INVALID_AMOUNT_MESSAGE = "Please pay correct amount!";
+	private final String TRANSACTION_SUCCESS_MESSAGE = "Transaction successful!";
+	private final String CHANGE_MESSAGE = "Change to return: ";
+	private final String INVALID_CARD_MESSAGE = "Invalid Card Number!";
 	
 	public Payment(double dueAmount){
 		setLayout(new GridLayout(6, 2));
-		
-		paymentMethods = new JComboBox(new String [] {"----", "CASH", "CARD"});
-		paymentMethods.addItemListener(this);
-		
-		cardNo = new JTextField();
-		cardNo.setEditable(false);
-		due = new JTextField();
+		paymentMethodsComboBox = new JComboBox<PaymentMethod>(PaymentMethod.values());
+		paymentMethodsComboBox.addItemListener(this);
+		cardNoField = new JTextField();
+		cardNoField.setEditable(false);
+		dueField = new JTextField();
 		dueAmount = Math.ceil(dueAmount);
-		due.setText(dueAmount + "");
-		due.setEditable(false);
-		paid = new JTextField();
-		paid.setEditable(false);
-		change = new JTextField();
-		change.setEditable(false);
-		
-		process = new JButton("Process");
-		process.addActionListener(this);
-		
-		add(new JLabel("Payment method"));
-		add(paymentMethods);
-		add(new JLabel("Card No."));
-		add(cardNo);
-		add(new JLabel("Due"));
-		add(due);
-		add(new JLabel("Paid"));
-		add(paid);
-		add(new JLabel("Change"));
-		add(change);
-		add(new JLabel());
-		add(process);
-		
-		// initialize frame properties
-		setSize(500, 400);
-		setTitle("Payment");
+		dueField.setText(dueAmount + "");
+		dueField.setEditable(false);
+		paidField = new JTextField();
+		paidField.setEditable(false);
+		changeField = new JTextField();
+		changeField.setEditable(false);
+		processButton = new JButton(PROCESS);
+		processButton.addActionListener(this);
+		add(new JLabel(BLANK));
+		add(paymentMethodsComboBox);
+		add(new JLabel(CARD_NO));
+		add(cardNoField);
+		add(new JLabel(DUE));
+		add(dueField);
+		add(new JLabel(PAID));
+		add(paidField);
+		add(new JLabel(CHANGE));
+		add(changeField);
+		add(new JLabel(BLANK));
+		add(processButton);
+		setSize(WIDTH, HEIGHT);
+		setTitle(TITLE);
 		setVisible(true);
-		setLocation(150, 50);
+		setLocation(LOCATION, LOCATION);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == process){ // Process button action
-			String method = paymentMethods.getSelectedItem().toString();
-			SaleDatabase sb = new SaleDatabase();
-			Sale sale = null;
+	public void actionPerformed(ActionEvent actionEvent) {
+		// "Process" is clicked.
+		if(actionEvent.getSource() == processButton){
+			// Determine payment method.
+			var paymentMethod = (PaymentMethod) paymentMethodsComboBox.getSelectedItem();
+			var saleDatabase = new SaleDatabase();
+			var due = Double.parseDouble(dueField.getText());
 			
-			if(method.equals("CASH")){ // CASH payment
-				double d = Double.parseDouble(due.getText());
-				double p = Double.parseDouble(paid.getText());
+			// Cash payment.
+			if(paymentMethod == PaymentMethod.Cash){
+				var paid = Double.parseDouble(paidField.getText());
 				
-				if(p < d){ // less amount paid, show error
-					JOptionPane.showMessageDialog(null, "Please pay correct amount! Thank you.");
-				}
-				
-				else{ // equal or more amount paid, calculate and display change if applicable
-					double c = p - d;
-					change.setText(c + "");
-					
-					if(p == d){ // no change
-						JOptionPane.showMessageDialog(null, "Transaction successful. Thank you!");
-						dispose();
-					}
-					
-					else{ // change
-						JOptionPane.showMessageDialog(null, "<html><h1>Please return change, amount: " + change.getText() + "</h1></html>");
-						dispose();
-					}
-					
-					// insert into sales table
-					sale = new Sale("", d);
-					sb.insert(sale);
-				}
-			}
-			
-			else if(method.equals("CARD")){ // CARD payment
-				String cNumber = cardNo.getText();
-				
-				if(cNumber.length() == 16){
-					JOptionPane.showMessageDialog(null, "Transaction successful. Thank you!");
-					// insert into sales table
-					sale = new Sale("", Double.parseDouble(due.getText()));
-					sb.insert(sale);
-					dispose();
+				// If paid amount is less than due.
+				if(paid < due){
+					JOptionPane.showMessageDialog(null, INVALID_AMOUNT_MESSAGE);
 				}
 				
 				else{
-					JOptionPane.showMessageDialog(null, "Invalid Card Number! Please check again.");
+					double change = paid - due;
+					changeField.setText(change + BLANK);
+					JOptionPane.showMessageDialog(null, CHANGE_MESSAGE + changeField.getText());
+					// Insert sale record into database.
+					var sale = new Sale(BLANK, due);
+					saleDatabase.insert(sale);
+					dispose();
+				}
+			}
+			
+			// Card payment.
+			else if(paymentMethod == PaymentMethod.Card){
+				var cardNo = cardNoField.getText();
+				
+				// If card no is valid.
+				if(cardNo.length() == 16){
+					JOptionPane.showMessageDialog(null, TRANSACTION_SUCCESS_MESSAGE);
+					// Insert sale record into database.
+					var sale = new Sale(BLANK, due);
+					saleDatabase.insert(sale);
+					dispose();
+				}
+				
+				// If card no is invalid.
+				else{
+					JOptionPane.showMessageDialog(null, INVALID_CARD_MESSAGE);
 				}
 			}
 		}
 	}
 
+	// Invoked when payment method is selected.
 	@Override
-	public void itemStateChanged(ItemEvent e) {
-		if(e.getStateChange() == 1){
-			String selectedMethod = e.getItem().toString();
-			
-			if(selectedMethod.equals("CASH")){ // CASH selected
-				cardNo.setEditable(false);
-				paid.setEditable(true);
-			}
-			
-			else if(selectedMethod.equals("CARD")){ // CARD selected
-				paid.setEditable(false);
-				cardNo.setEditable(true);
-			}
+	public void itemStateChanged(ItemEvent itemEvent) {
+		if(itemEvent.getStateChange() == 1){
+			// Determine payment method.
+			var paymentMethod = (PaymentMethod) itemEvent.getItem();
+			System.out.println(paymentMethod);
+			boolean isCashPayment = (paymentMethod == PaymentMethod.Cash) ? true : false;
+			cardNoField.setEditable(!isCashPayment);
+			paidField.setEditable(isCashPayment);
 		}
 	}
 }
